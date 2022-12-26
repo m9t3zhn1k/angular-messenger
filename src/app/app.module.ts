21fbc22c377b8754aedfdc769339493479ad26e5
m9t3zhn1k <m9t3zhn1k@mail.ru>
2022-12-27 01:50:42 +0300

    add taiga-ui, config prettier, add basic styles and media

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TuiRootModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TuiRootModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
